70e45a98b9c2354931cb1b59ebf4568d7d46ebda
Uanderson <uanderson.lm@gmail.com>
2024-03-08 09:06:34 -0300

    Ajustando desempenho para  navegação

--- src/Components/Button_curriculum/index.jsx
+++ src/Components/Button_curriculum/index.jsx
@@ -1,11 +1,11 @@
-import { Box_Button } from "./style";
+// import { Box_Button } from "./style";
 
 export const Component_Button_Curriculum = () => {
   return (
     <a href="https://drive.google.com/file/d/1gi7wo_X-Q_IsDf0VjVZmq4gntrpqbmk4/view?usp=sharing" target="_blank">
-      <Box_Button>
+      <div>
         <img src="assets/icones-linguagens/icone_download.png" alt="PDF"/>
-      </Box_Button>
+      </div>
     </a>
   );
 };
