d36f334091a8e83f7a9f0149b535815ff6de0386
Uanderson <uanderson.lm@gmail.com>
2024-03-08 09:23:50 -0300

    Atualização de branch main com alterações da develop

--- src/Components/Button_curriculum/index.jsx
+++ src/Components/Button_curriculum/index.jsx
@@ -4,7 +4,7 @@ export const Component_Button_Curriculum = () => {
   return (
     <a href="https://drive.google.com/file/d/1gi7wo_X-Q_IsDf0VjVZmq4gntrpqbmk4/view?usp=sharing" target="_blank">
       <div>
-        <img src="assets/icones-linguagens/icone_download.png" alt="PDF"/>
+        <img src="assets/icones-linguagens/icone_download.png" alt="icone PDF"/>
       </div>
     </a>
   );
